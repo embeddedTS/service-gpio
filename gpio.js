72df8733e5e4192c03c6051066c1375be5883c88
Michael Schmidt <michael@ix.ts-local.net>
2016-12-05 11:49:15 -0700

    implemented GPIO server logic using sysfs

--- gpio.js
+++ gpio.js
@@ -1,31 +1,70 @@
 "use strict";
 
+var fs = require("fs")
 var app = require("server")().app
 var endpoint = "/gpio/"
 
+function TryWrite(path,str,success,failure) {
+    try {
+	var out=fs.createWriteStream(path)
+	out.on("finish",success)
+	out.write(str)
+	out.end()
+    } catch (e) {
+	if (failure) failure(e)
+    }
+}
+
+function gpio_action(res) {
+    if (url.length == 2) {
+	var dir=fs.readFileSync(path+"/direction", "utf8")
+	var data=Number(fs.readFileSync(path+"/value", "utf8"))
+	if (dir == "out") {
+	    if (data) {
+		return res.send("HIGH")
+	    } else {
+		return res.send("LOW")
+	    }
+	} else { // dir == "in"
+	    if (data) {
+		return res.send("INPUT_HIGH")
+	    } else {
+		return res.send("INPUT_LOW")
+	    }
+	}
+    } else { // url.length == 3
+	var val=url[2],cmd
+	switch (val) {
+	case "INPUT": cmd="in"; break
+	case "HIGH": cmd="high"; break
+	case "LOW": cmd="low"; break
+	default: return res("ERROR")
+	}
+	TryWrite(path+"/direction",cmd,function() {
+	    return res.send("OK")
+	}, function() {
+	    return res.send("ERROR")
+	})
+    }
+}
+
 function gpio(req,res,next) {
     var num,val,url = req.path.split("/")
-    if (url.length == 2) { // GET /gpio/#
-	num=url[1]
-	res.send("HIGH")
-    } else if (url.length == 3) { // GET /gpio/#/VALUE
-	num=url[1]
-	val=url[2]
-	switch (val) {
-	case "INPUT":
-	    res("OK"); break
-	case "HIGH":
-	    res("OK"); break
-	case "LOW":
-	    res("OK"); break
-	default:
-	    res("ERROR"); break
-	}
-	
+    console.log(req.path)
+    if (url.length < 2 || url.length > 3) return res.send("ERROR")
+    num=Number(url[1])
+    if (num < 0) return res.send("ERROR")
+    var path="/sys/class/gpio/gpio"+num
+    if (!fs.existsSync(path)) {
+	TryWrite(path+"/export",""+num,function() {
+	    if (!fs.existsSync(path)) return res.send("ERROR") // what happened?
+	    gpio_action(res)
+	}, function() {
+	    return res.send("ERROR") // bad GPIO number?
+	})
     } else {
-	res.send("ERROR")
+	gpio_action(res)
     }
-    console.log(url)
 }
 
 /*
